aed9cf0e2b949f0a91f1dc9a90dd68ba0b06bc84
[ Shreyans06 ] <shreyanskjain06@gmail.com>
2023-10-12 23:24:47 -0700

    Added dataloader file

---
+++ dataloader.py
@@ -0,0 +1,78 @@
+from torch.utils.data import Dataset as BaseDataset
+import os
+import cv2
+import numpy as np
+
+
+class ChestXDetDataset(BaseDataset):
+    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
+    
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline 
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing 
+            (e.g. noralization, shape manipulation, etc.)
+    
+    """
+    
+    CLASSES = ['no finding', 'atelectasis', 'calcification', 'cardiomegaly', 'consolidation', 'diffuse nodule', 'effusion', 'emphysema', 'fibrosis', 'fracture', 'mass', 'nodule', 'pleural thickening', 'pneumothorax']
+
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir,
+            dim=(224, 224, 3), 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.dim = dim
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+        
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        input_rows = self.dim[0]
+        input_cols = self.dim[1]
+
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], cv2.IMREAD_GRAYSCALE)
+
+        image = cv2.resize(image, (input_rows, input_cols), interpolation=cv2.INTER_NEAREST)
+        mask = cv2.resize(mask, (input_rows, input_cols), interpolation=cv2.INTER_NEAREST)
+        
+        # print(self.ids[i])
+        # extract certain classes from mask (e.g. cars)
+        # plt.imsave(f"abb.png" , mask , cmap ='gray')
+
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1)
+        # print(mask.shape)
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        return len(self.ids)
+
